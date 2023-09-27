f57f2ce98fb27bbb91cc6397623579791243f7e4
klenovich <klucas12345@gmail.com>
2023-09-27 04:57:41 +0000

    Models 3/

---
+++ models/IndexModel.cs
@@ -0,0 +1,7 @@
+public class IndexModel : PageModel
+{
+    public List<Stock> Stocks { get; set; }
+    public List<Trade> Trades { get; set; }
+
+    // ...additional properties and methods as needed...
+}
