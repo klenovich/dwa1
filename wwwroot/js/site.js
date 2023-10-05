b8c9dc38f7ad489a4641d3dc838d53523641d2b4
klenovich <89214539+klenovich@users.noreply.github.com>
2023-10-05 03:26:29 +0000

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,7 @@
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
 // Your JavaScript code can go here
 
 // Document Ready Event
@@ -14,6 +15,21 @@ $(document).ready(function() {
     $('#myButton').on('click', function() {
         // Code to run when the button is clicked
         alert('Button clicked!');
+    });
+
+    // Example: Toggle a CSS class on an element
+    $('.toggle-element').on('click', function() {
+        $(this).toggleClass('active');
+    });
+
+    // Example: Show a hidden element
+    $('#showElementButton').on('click', function() {
+        $('#hiddenElement').show();
+    });
+
+    // Example: Hide an element
+    $('#hideElementButton').on('click', function() {
+        $('#hiddenElement').hide();
     });
 
     // You can add more code and functionality here
@@ -46,4 +62,42 @@ function customFunction(param1, param2) {
     return result;
 }
 
-// You can define more functions and add your JavaScript code as needed
+// Example AJAX Request
+function fetchData() {
+    // Make an AJAX request to an API
+    $.ajax({
+        url: '/api/data',
+        method: 'GET',
+        success: function(data) {
+            // Handle the data returned from the API
+            console.log(data);
+        },
+        error: function(error) {
+            // Handle any errors that occur during the request
+            console.error('Error:', error);
+        }
+    });
+}
+
+// Example Form Submission
+$('#myForm').submit(function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Gather form data
+    var formData = $(this).serialize();
+
+    // Submit the data via AJAX
+    $.ajax({
+        url: '/api/submitForm',
+        method: 'POST',
+        data: formData,
+        success: function(response) {
+            // Handle the response from the server
+            console.log('Form submitted successfully:', response);
+        },
+        error: function(error) {
+            // Handle any errors that occur during the request
+            console.error('Error:', error);
+        }
+    });
+});
